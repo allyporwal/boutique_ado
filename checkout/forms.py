ce9e56e50a0b8c3244cac4f2dfea84a662fd6e4b
allyporwal <allyporwal@gmail.com>
2021-02-26 12:21:09 +0000

    Add email to checkout process

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,7 +3,7 @@ from .models import Order
 
 
 class OrderForm(forms.ModelForm):
-    class Meta():
+    class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
