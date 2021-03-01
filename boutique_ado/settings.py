5dde1c324a054ffa6f0e64649e3d7f107b153fea
allyporwal <allyporwal@gmail.com>
2021-03-01 14:27:54 +0000

    Fix secret key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4qky$j(v^#857vgq76&7v^r(&2$92=t3zbhl25f0@35u-0m@+3'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ajp-boutique-ado.herokuapp.com', 'localhost']
 
